407592fc9e0573436fcfc3a8ec29ceb79a421265
Sugambaj7 <sugambaj123@gmail.com>
2023-08-07 18:31:16 +0545

    demo of recycler view and used custom adapter to inflate views and used custom dialog box to pop up when user clicks on image

---
+++ app/src/main/java/com/example/listview_gridview_recyclerview/ImageModelRecyclerView.java
@@ -0,0 +1,2 @@
+package com.example.listview_gridview_recyclerview;public class ImageModelRecyclerView {
+}
---
+++ app/src/main/java/com/example/listview_gridview_recyclerview/RecyclerViewImageAdapter.java
@@ -0,0 +1,5 @@
+package com.example.listview_gridview_recyclerview;
+
+public class RecyclerViewAdapter {
+    
+}
